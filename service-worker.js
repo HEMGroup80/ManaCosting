a17336189963edd52c659f5619fb868190332f6e
HEMGroup80 <hesamalvandi1380@gmail.com>
2026-02-20 11:59:27 +0330

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,13 +1,14 @@
 // Service Worker ساده برای آفلاین (Cache-First)
-const CACHE_NAME = "costing-pwa-v1";
+const CACHE_NAME = "costing-pwa-v2";
 const PRECACHE_URLS = [
   "./",
   "./index.html",
   "./manifest.webmanifest",
   "./service-worker.js",
-  "./icons/icon-192.png",
-  "./icons/icon-512.png"
+  "./icons/mana-192.png",
+  "./icons/mana-512.png"
 ];
+
 
 self.addEventListener("install", (event) => {
   event.waitUntil(
